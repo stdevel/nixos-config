724f572b1d90123b222292b6a48558231d26bfba
Christian Stankowic <info@cstan.io>
2023-05-01 18:17:05 +0200

    fix user configuration

--- users/christian.nix
+++ users/christian.nix
@@ -7,6 +7,6 @@
       "dialout"
     ];
     shell = pkgs.bash;
-    hashedPassword = "$y$j9T$zyjvSwv.Lf4g8UMR4j6rW1$pzhgtm8CHxNQB4bIhbGUSteDqW9T/3Oz7G2ku69Dac6:19478";
+    hashedPassword = "$y$j9T$zyjvSwv.Lf4g8UMR4j6rW1$pzhgtm8CHxNQB4bIhbGUSteDqW9T/3Oz7G2ku69Dac6";
   };
 }
